3d776c481177491d4fd1d715ff2f88716da59144
AccaEmme <AccaEmme@users.noreply.github.com>
2019-12-10 16:40:44 +0100

    improvement "state toggle"
    improvement "state toggle".
    i.g. from
       if (lettera == 'E') {
        lettera=' ';
        if ( statoStudio == 0) {
          digitalWrite(pinStudio, HIGH);
          statoStudio = 1;
        } else {
          digitalWrite(pinStudio, LOW);
          statoStudio = 0;
        }
        Serial.flush();
      }
    to
      if (lettera == 'E') {
        lettera=' ';
        digitalWrite( pinStudio, !digitalRead(pinStudio) );
        Serial.println("Stato Studio: " + (String) digitalRead(pinStudio));
        Serial.flush();
      }

--- ArduinoUNO.ino
+++ ArduinoUNO.ino
@@ -1,15 +1,15 @@
 char lettera;
-int statoEntrata = 0;
-int  statoCucina = 0;
-int  statoStanza = 0;
-int statoSoffitta = 0;
-int statoStudio = 0;
+int  statoEntrata  = 0;
+int  statoCucina   = 0;
+int  statoStanza   = 0;
+int  statoSoffitta = 0;
+int  statoStudio   = 0;
 
-const int pinSoffitta = 9;
-const int pinStanza = 12;
-const int pinEntrata = 10;
-const int pinStudio = 11;
-const int pinCucina = 8;
+const int pinSoffitta =  9;
+const int pinStanza   = 12;
+const int pinEntrata  = 10;
+const int pinStudio   = 11;
+const int pinCucina   =  8;
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,80 +27,48 @@ void loop() {
   while (Serial.available() > 0) {
     lettera = Serial.read();
     Serial.println(lettera);
-    Serial.println("Entrata: " + (String)statoEntrata);
-    Serial.println("Cucina: " + (String)statoCucina);
-    Serial.println("Stanza: " + (String)statoStanza);
+    Serial.println("Entrata: "  + (String)statoEntrata);
+    Serial.println("Cucina: "   + (String)statoCucina);
+    Serial.println("Stanza: "   + (String)statoStanza);
     Serial.println("soffitta: " + (String)statoSoffitta);
-    Serial.println("Studio: " + (String)statoStudio);
+    Serial.println("Studio: "   + (String)statoStudio);
   }
 
   if (lettera == 'A') {
     lettera=' ';
-    if (statoEntrata == 0) {
-      digitalWrite(pinEntrata, HIGH);
-      statoEntrata = 1;
-      Serial.println("Entrata: " + (String)statoEntrata);
-    } else {
-      digitalWrite(pinEntrata, LOW);
-      statoEntrata = 0;
-      Serial.println("Entrata: " + (String)statoEntrata);
-    }
+    digitalWrite( pinEntrata, !digitalRead(pinEntrata) );
+    Serial.println("Stato Entrata: " + (String) digitalRead(pinEntrata));
     Serial.flush();
   }
 
-
-
   if (lettera == 'B') {
     lettera=' ';
-    if (statoCucina == 0) {
-      digitalWrite(pinCucina, HIGH);
-      statoCucina = 1;
-    } else {
-      digitalWrite(pinCucina, LOW);
-      statoCucina = 0;
-
-    }
+    digitalWrite( pinCucina, !digitalRead(pinCucina) );
+    Serial.println("Stato Cucina: " + (String) digitalRead(pinCucina));
     Serial.flush();
   }
 
 
   if (lettera == 'C') {
     lettera=' ';
-    if (statoStanza == 0) {
-      digitalWrite(pinStanza, HIGH);
-      statoStanza = 1;
-    }
-
-    else {
-      digitalWrite(pinStanza, LOW);
-      statoStanza = 0;
-    }
+    digitalWrite( pinStanza, !digitalRead(pinStanza) );
+    Serial.println("Stato Stanza: " + (String) digitalRead(pinStanza));
     Serial.flush();
   }
 
 
   if (lettera == 'D') {
     lettera=' ';
-    if (statoSoffitta == 0) {
-      digitalWrite(pinSoffitta, HIGH);
-      statoSoffitta = 1;
-    } else {
-      digitalWrite(pinSoffitta, LOW);
-      statoSoffitta = 0;
-    }
+    digitalWrite( pinSoffitta, !digitalRead(pinSoffitta) );
+    Serial.println("Stato Soffitta: " + (String) digitalRead(pinSoffitta));
     Serial.flush();
   }
 
 
   if (lettera == 'E') {
     lettera=' ';
-    if ( statoStudio == 0) {
-      digitalWrite(pinStudio, HIGH);
-      statoStudio = 1;
-    } else {
-      digitalWrite(pinStudio, LOW);
-      statoStudio = 0;
-    }
+    digitalWrite( pinStudio, !digitalRead(pinStudio) );
+    Serial.println("Stato Studio: " + (String) digitalRead(pinStudio));
     Serial.flush();
   }
 }
